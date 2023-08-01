0d214f79ae782dc5cf77ffae8249814b8acf183d
YounisSaeed <youniszidan94@gmail.com>
2023-08-01 21:31:31 +0300

    add class authenticateuser before using any principles special --SRP

---
+++ solid/Single-Responsability/before.ts
@@ -0,0 +1,37 @@
+// bad example of oop before using SRP
+
+class AuthenticationService {
+    private users: Map<string, {username: string; password:string}> = new Map();
+
+    constructor() {
+        this.users.set('user1', {username: 'user1' , password: 'password1'});
+        this.users.set('user1', {username: 'user1' , password: 'password1'});
+    }
+
+    public authenticate(username:string , password: string ): boolean {
+        const user = this.users.get(username);
+        if (!user) {
+            return false;
+        }
+        return user.password === password;
+    }
+
+    public changePassword(username: string , newPassword: string): boolean {
+        const user = this.users.get(username);
+        if (!user){
+            return false;
+        }
+        user.password = newPassword
+        return true;
+    }
+    public addUser(username:string,password:string): boolean {
+        if (this.users.has(username)) {
+            return false;
+        }
+        this.users.set(username, {username,password});
+        return true;
+    }
+    public removeUser(username: string):boolean {
+        return this.users.delete(username);
+    }
+}
